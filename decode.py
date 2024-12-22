79200f6c6e8af97f3308909ca37788cf531315e9
長谷川悠人 <yy6056anderson@outlook.com>
2024-12-22 18:43:29 +0800

    Delete decode.py

--- decode.py
+++
@@ -1,4 +0,0 @@
-import urllib.parse
-encoded_url = 'https://news.mingpao.com/ins/%e7%86%b1%e9%bb%9e/article/20241210/s00024/1733573709632'
-decoded_url = urllib.parse.unquote(encoded_url)
-print('Decoded URL:', decoded_url)
